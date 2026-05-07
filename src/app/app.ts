f5bf65a73683b6251fcfebf056e91c5e1c8b7c86
Nathan Bowman <gr8n8bowman1@gmail.com>
2026-05-07 04:36:21 -0500

    Added Material toolbar navigation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink, Router } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, MatToolbarModule, MatButtonModule, MatCardModule, MatIconModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
