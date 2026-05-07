9a6fc933de5978b851031361524510d714865e58
Nathan Bowman <gr8n8bowman1@gmail.com>
2026-05-07 04:56:16 -0500

    Configured firebase into Angular app

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyB_nyiHLAM51-nOctRvHiYJu5A5InPYFYo",
+    authDomain: "expense-tracker-firebase-5e8c8.firebaseapp.com",
+    projectId: "expense-tracker-firebase-5e8c8",
+    storageBucket: "expense-tracker-firebase-5e8c8.firebasestorage.app",
+    messagingSenderId: "143118412753",
+    appId: "1:143118412753:web:98676357e65242f76dcef0"
+  }
+};
