2106fa65737a654b972c4522fbac335201a13318
Nathan Bowman <gr8n8bowman1@gmail.com>
2026-05-07 07:15:28 -0500

    Made the expense items into cards

--- src/app/components/expense-item/expense-item.ts
+++ src/app/components/expense-item/expense-item.ts
@@ -2,11 +2,13 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
 import { Expense } from "../../models/expense";
 import { RouterLink } from "@angular/router";
 import { CommonModule } from "@angular/common";
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @Component({
     selector: 'app-expense-item',
-    imports: [RouterLink, CommonModule],
+    imports: [RouterLink, CommonModule, MatCardModule, MatButtonModule],
     templateUrl:'./expense-item.html'
 })
 export class ExpenseItemComponent{
