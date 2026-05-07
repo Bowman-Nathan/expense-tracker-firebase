b655b27fbba8c680617c77c5521b56015d7569b0
Nathan Bowman <gr8n8bowman1@gmail.com>
2026-05-07 09:27:37 -0500

    Added the filtering to the expenses

--- src/app/components/expense-list/expense-list.ts
+++ src/app/components/expense-list/expense-list.ts
@@ -1,15 +1,39 @@
-import { Component } from "@angular/core";
+import { Component, computed, signal } from "@angular/core";
 import { ExpenseService } from "../../services/expense-service";
 import { ExpenseItemComponent} from '../expense-item/expense-item';
+import { FormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
-    selector: 'app-expense-list',
-    imports:[ExpenseItemComponent],
-    templateUrl: './expense-list.html'
+  selector: 'app-expense-list',
+  standalone: true,
+  imports: [
+    ExpenseItemComponent,
+    FormsModule,
+    MatSelectModule,
+    MatFormFieldModule
+  ],
+  templateUrl: './expense-list.html'
 })
-export class ExpenseListComponent{
-    constructor(public expenseService: ExpenseService) {}
-    deleteExpense(id: string){
-        this.expenseService.deleteExpense(id);
+export class ExpenseListComponent {
+  selectedCategory = signal('All');
+
+  constructor(public expenseService: ExpenseService) {}
+
+  filteredExpenses = computed(() => {
+    if (this.selectedCategory() === 'All') {
+      return this.expenseService.expenses();
     }
+
+    return this.expenseService
+      .expenses()
+      .filter(expense => expense.category === this.selectedCategory());
+  });
+
+  expenses = this.filteredExpenses;
+
+  deleteExpense(id: string) {
+    this.expenseService.deleteExpense(id);
+  }
 }
