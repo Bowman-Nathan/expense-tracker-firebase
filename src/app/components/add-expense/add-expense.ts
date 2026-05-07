7cd947ffb69944d7bed6d6ddb7cd9402f025e4dc
Nathan Bowman <gr8n8bowman1@gmail.com>
2026-05-07 04:48:25 -0500

    Convered the add expense style to angular material

--- src/app/components/add-expense/add-expense.ts
+++ src/app/components/add-expense/add-expense.ts
@@ -2,10 +2,15 @@ import { Component } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { ExpenseService } from "../../services/expense-service";
 import { ExpenseCategory } from "../../models/expense";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from "@angular/material/card";
 
 @Component({
     selector: 'app-add-expense',
-    imports: [FormsModule],
+    imports: [FormsModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatButtonModule, MatCardModule],
     templateUrl: './add-expense.html'
 })
 export class AddExpenseComponent {
