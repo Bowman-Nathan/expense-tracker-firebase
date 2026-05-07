cd5a381816f9cf66cab8dd2a6ddbc1f52fd510ea
Nathan Bowman <gr8n8bowman1@gmail.com>
2026-05-07 09:49:12 -0500

    added dashboard chart analytics

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -1,12 +1,19 @@
 import { Component } from "@angular/core";
 import { ExpenseService } from "../../services/expense-service";
 import { MatCardModule } from "@angular/material/card";
+import { BaseChartDirective } from 'ng2-charts'
+import { Chart, registerables } from "chart.js";
+
+Chart.register(...registerables);
 
 @Component({
-    selector: 'app-dashboard',
-    imports: [MatCardModule],
-    templateUrl: './dashboard.html'
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [MatCardModule, BaseChartDirective],
+  templateUrl: './dashboard.html'
 })
 export class DashboardComponent {
-    constructor(public expenseService: ExpenseService) {}
+
+  constructor(public expenseService: ExpenseService) {}
+
 }
